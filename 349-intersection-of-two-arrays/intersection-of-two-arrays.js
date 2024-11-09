61e53f56e91fc7c075ad43f67a8ff865b80ff8ae
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2024-11-09 15:41:41 -0600

    Time: 0 ms (100.00%) | Memory: 51.1 MB (24.73%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -7,6 +7,5 @@ var intersection = function(nums1, nums2) {
     let set1 = new Set(nums1);
     let set2 = new Set(nums2);
     let result = [...set1].filter(item => set2.has(item));
-    console.log(result);
     return result;
 };
