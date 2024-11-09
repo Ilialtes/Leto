7e8cf0525eff49ca88e7a720b4ac6690d8c636d8
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2024-11-09 17:27:10 -0600

    Time: 0 ms (100.00%) | Memory: 50.3 MB (68.77%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,8 +4,6 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let set1 = new Set(nums1);
-    let set2 = new Set(nums2);
-    let result = [...set1].filter(item => set2.has(item));
-    return result;
+    let result = nums1.filter(item => nums2.includes(item))
+    return [...new Set(result)];
 };
