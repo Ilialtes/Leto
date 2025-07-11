9e4d8d006e18235e565ea96a18837b26c3d179d9
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-07-11 13:22:43 -0600

    Time: 533 ms (5.15%) | Memory: 81.4 MB (5.23%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,12 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    seenNumbers = {}
-    for(num of nums){     
-        if(seenNumbers[num]) {
-            return true
+    newNums = {}
+    for(num of nums){
+        newNums[num] = newNums[num] + 1 || 1;
+        if(newNums[num] == 2) {
+            return true 
         }
-        seenNumbers[num] = true
+        console.log(newNums[num])
     }  
     return false
 };
