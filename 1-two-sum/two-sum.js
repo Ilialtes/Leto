0a161815a8e9b07af1bf4e4045686a7e1e0e95eb
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-08-11 14:21:29 -0600

    Time: 33 ms (34.68%) | Memory: 56.1 MB (20.47%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map()
-    for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
-        if(map.has(complement)) {
-            return [map.get(complement),i]
+    if(nums.length === 2) return [0,1]
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                return[i , j]
+            }  
         }
-        map.set(nums[i],i)
-    } 
-    return map
-}
+    }
+};
