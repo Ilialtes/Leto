61b24bf24a6cbf824462ac71d68f88a5058db7aa
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-08-11 13:47:17 -0600

    Time: 13 ms (66.28%) | Memory: 58 MB (53.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-
-   return s.split('').sort().join('') === t.split('').sort().join('')
+    if(s.length !== t.length) return false 
+    let sMap = createMap(s)
+    let tMap = createMap(t)
+    console.log(sMap)
+    console.log(tMap)
+    
+    for(char in sMap) {
+        if(sMap[char] !== tMap[char]){
+            return false
+        } 
+    }
+    return true
+   //return s.split('').sort().join('') === t.split('').sort().join('')
 };
+
+function createMap(y) {
+    let inMap = {}
+    for(char of y) {
+        inMap[char] = inMap[char] + 1 || 1
+    }
+    return inMap
+}
