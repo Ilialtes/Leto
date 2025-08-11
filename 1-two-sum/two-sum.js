246d7fdc0d10e5fb214328991a7fb57131f1d791
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-08-11 14:24:42 -0600

    Time: 32 ms (41.01%) | Memory: 56 MB (22.68%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    
     if(nums.length === 2) return [0,1]
     for(let i = 0; i < nums.length; i++) {
         for(let j = i+1; j < nums.length; j++) {
