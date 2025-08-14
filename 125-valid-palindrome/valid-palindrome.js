1ebe79060098ae11f41eac1efad79a36ed6337be
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-08-13 18:08:48 -0600

    Time: 5 ms (64.42%) | Memory: 59.9 MB (32.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,5 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    if(s.length === 0) return false
+
     return (s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') === s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '').split('').reverse().join(''))
+    
 };
