2d29305a0b2eb3b9962cfacee378bda0c2271442
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-08-21 10:10:42 -0600

    Time: 35 ms (70.51%) | Memory: 72.9 MB (33.53%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -9,7 +9,7 @@ var threeSum = function(nums) {
     
     for(let i = 0; i < nums.length-2; i++) {
         //stop duplicates
-        if(i>0 && nums[i]=== nums[i-1]) continue;
+        if(i>0 && nums[i] === nums[i-1]) continue;
         let left = i+1
         let right = nums.length - 1
 
@@ -17,8 +17,8 @@ var threeSum = function(nums) {
             let sum = nums[i] + nums[left] + nums[right]
             if(sum === 0) {
                 result.push([nums[i],nums[left],nums[right]])
-                while(nums[left] === nums[left+1]) left++
-                while(nums[right] === nums[right-1]) right--
+                while(left < right && nums[left] === nums[left+1]) left++
+                while(left < right && nums[right] === nums[right-1]) right--
                 left++
                 right--
             }else if(sum <0) {
