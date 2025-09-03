0de3858235cd45c0925d8f506d2b5428aaa1f982
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-09-03 16:37:13 -0600

    Time: 38 ms (37.92%) | Memory: 68.4 MB (38.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -1,24 +1,20 @@
-
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    const wordKeys = {}
-
-    if (strs.length === 1) return [[...strs]]
-
-    for (str of strs) {
-    const charCount = Array(26).fill(0)
-        for(char of str) {
-            let index = char.charCodeAt(0) - 'a'.charCodeAt(0)
-            charCount[index]++
+var groupAnagrams = function(strs) {
+    let anagramGroup= {}
+        for(let str of strs) {
+            let charCount = Array(26).fill(0)
+            for(let char of str) {
+                let index = char.charCodeAt(0) - 'a'.charCodeAt(0)
+                charCount[index]++
+            }
+            let key = charCount.join('#')
+            if(!anagramGroup[key]) {
+                anagramGroup[key] = []
+            }
+            anagramGroup[key].push(str)
         }
-        const key = charCount.join('#')
-        if (!wordKeys[key]) {
-            wordKeys[key] = []
-        }
-        wordKeys[key].push(str)
-    }
-    return Object.values(wordKeys)
-};
+        return Object.values(anagramGroup)
+}
