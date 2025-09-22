003d5de8c2d87cda7440018858f90b69c9ee14e3
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-09-22 08:43:59 -0600

    Time: 12 ms (9.21%) | Memory: 59.9 MB (21.86%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,32 +3,22 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
+    let cordinates = []
     let numRows = matrix.length;
     let numCols = matrix[0].length;
-    let cordinates = []
-    for(let i = 0; i < matrix.length; i++) {
-        for(let j = 0; j < matrix[i].length; j++) {
-            if(matrix[i][j] === 0) {
-                cordinates.push({ x: i, y: j });
-            }
+
+    for(let i = 0; i < numRows; i++) {
+        for(let j = 0; j < numCols; j++){
+            if (matrix[i][j] === 0) cordinates.push({x:i,y:j})
         }
     }
-    for(cordinate of cordinates) {
-        fillCol(cordinate, matrix)
-        fillRow(cordinate, matrix)
-    }
-};
-
-let fillCol = (cordinate, matrix) => {  
-    let numRows = matrix.length;
-    for(let i = 0; i < numRows; i++) {
-        matrix[i][cordinate.y] = 0
-    }
-}
-
-let fillRow = (cordinate,matrix) => {
-    let numCols = matrix[0].length;
-    for(let i = 0; i < numCols; i++) {
-        matrix[cordinate.x][i] = 0
+    console.log(cordinates)
+    for(const cordinate of cordinates) {
+        for(let i = 0; i < numRows; i++) {
+            matrix[i][cordinate.y] = 0 
+        } 
+        for(let j = 0; j < numCols; j++) {
+            matrix[cordinate.x][j] = 0 
+        }
     }
 }
