19616a757e2dc58491c385c3e2f52aa43a32c8b3
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-09-22 14:02:47 -0600

    Time: 6 ms (26.94%) | Memory: 60.1 MB (12.74%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -4,21 +4,22 @@
  */
 var setZeroes = function(matrix) {
     let cordinates = []
-    let numRows = matrix.length;
-    let numCols = matrix[0].length;
-
-    for(let i = 0; i < numRows; i++) {
-        for(let j = 0; j < numCols; j++){
-            if (matrix[i][j] === 0) cordinates.push({x:i,y:j})
+    let numCol = matrix[0].length
+    let numRow = matrix.length 
+    
+    for(let i = 0; i < numRow; i++ ) {
+        for(let j = 0; j < numCol; j++){
+            if(matrix[i][j]=== 0) {
+                cordinates.push({x:i, y: j})
+            }
         }
     }
-    console.log(cordinates)
-    for(const cordinate of cordinates) {
-        for(let i = 0; i < numRows; i++) {
-            matrix[i][cordinate.y] = 0 
-        } 
-        for(let j = 0; j < numCols; j++) {
-            matrix[cordinate.x][j] = 0 
+    for(cordinate of cordinates) {
+        for(let i = 0; i < numRow; i++) {
+            matrix[i][cordinate.y] = 0
+        }
+        for(let j = 0; j < numCol; j++) {
+            matrix[cordinate.x][j] = 0
         }
     }
 }
