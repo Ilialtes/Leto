02b1287b60210f66246de62df1fbd5004b69dbbb
JOSUE PEREZ VALVERDE <JOSUE.PEREZVALVERDE@UCR.AC.CR>
2025-09-29 19:43:27 -0600

    Time: 66 ms (12.55%) | Memory: 64.3 MB (5.69%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,18 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // FIX 1: Always declare your variables with const, let, or var.
-    const seenComplement = {};
-
-    for (let i = 0; i < nums.length; i++) {
-        const thisNumber = nums[i];
-        const complement = target - thisNumber;
-
-        if (seenComplement[complement] !== undefined) {
-            // FIX 2: You found the complement, so use it as the key to get its stored index.
-            return [seenComplement[complement], i];
+    let seen = {}
+    for(let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        console.log(complement)
+        if(seen[complement] !== undefined) {
+            return [seen[complement], i]
         }
-
-        seenComplement[thisNumber] = i;
+        seen[nums[i]] = i
     }
 };
